151f88d16a0916e3aedf16cf528b2a69518e9d1c
Donghan Lyu <DonghanLyu@outlook.com>
2025-11-11 14:14:08 +0800

    Initial Commit

---
+++ BeneDict/BeneDictApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeneDictApp.swift
+//  BeneDict
+//
+//  Created by Donghan Lyu on 2025/11/11.
+//
+
+import SwiftUI
+
+@main
+struct BeneDictApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BeneDict/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BeneDict
+//
+//  Created by Donghan Lyu on 2025/11/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
